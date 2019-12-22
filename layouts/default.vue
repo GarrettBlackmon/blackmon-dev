4a1ed138ef1a35d498bbee320d411eef1d62be4c
Garrett <garrett@yellowcard.io>
2019-12-22 03:57:48 -0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-container fill-height>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+::-webkit-scrollbar {
+  width: 3px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-layout row wrap justify-center align-center>
+    <vue-particles
+      :clickEffect="false"
+      :hoverEffect="false"
+      :moveSpeed="2"
+      color="#fff"
+    />
+    <v-flex xs8>
+      <v-card>
+        <v-card-title>Garrett Blackmon</v-card-title>
+        <v-card-subtitle>Full Stack Developer</v-card-subtitle>
+        <v-card-text
+          >Software developer currently based in Atlanta Georgia interested in
+          building robust, reliable, efficient, and most of all beautiful
+          websites and webapps. Open source hippie. Experience in VueJs,
+          MongoDB, NuxtJs, Serverless Framework</v-card-text
+        >
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn icon :href="links.github">
+            <v-icon>fab fa-github</v-icon>
+          </v-btn>
+
+          <v-btn icon :href="links.linkedin">
+            <v-icon>fab fa-linkedin-in</v-icon>
+          </v-btn>
+
+          <v-btn icon :href="links.twitter">
+            <v-icon>fab fa-twitter</v-icon>
+          </v-btn>
+
+          <v-btn icon :href="links.email">
+            <v-icon>fa fa-envelope</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: {
+        github: 'https://github.com/GarrettBlackmon',
+        linkedin: 'https://www.linkedin.com/in/garrettblackmon/',
+        twitter: 'https://twitter.com/GarrettBlackmon',
+        email: 'mailto:garrett@blackmon.dev'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#particles-js {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 0;
+}
+</style>
