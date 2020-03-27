a740ea4503d8606115ffe0d14e593a5973b1bf35
Garrett <garrett@yellowcard.io>
2020-03-26 21:18:11 -0500

    Added snackbar for dark mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,9 +35,12 @@
         <v-btn text class="mx-5">{{ item.title }}</v-btn>
       </v-toolbar-items>
       <v-spacer></v-spacer>
-      <v-btn text @click.stop="goDark = !goDark">
+      <v-btn fab small text @click.stop="goDark = !goDark">
         <v-icon>mdi-moon-waxing-crescent</v-icon>
       </v-btn>
+      <v-snackbar right vertical v-model="snackbar" :timeout="2000">
+        This website supports Dark Mode!
+      </v-snackbar>
     </v-app-bar>
     <v-content>
       <v-container fluid pa-0 ma-0>
@@ -57,6 +60,7 @@ export default {
     return {
       drawer: false,
       goDark: false,
+      snackbar: false,
       items: [
         {
           icon: 'mdi-file-document',
@@ -84,8 +88,25 @@ export default {
         return (this.$vuetify.theme.dark = false)
       }
     }
+  },
+  methods: {
+    activateSnack() {
+      setTimeout(() => (this.snackbar = true), 1000)
+    }
+  },
+  mounted() {
+    this.activateSnack()
   }
 }
 </script>
 
-<style></style>
+<style>
+/* Vuetify overrides */
+.v-snack__wrapper {
+  min-width: 0;
+  margin-right: 60px !important;
+}
+.v-snack .v-snack--active .v-snack--bottom .v-snack--right .v-snack--vertical {
+  margin-top: 20px !important;
+}
+</style>
