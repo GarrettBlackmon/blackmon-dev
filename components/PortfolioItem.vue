b7c7c76ca44b7a305420dc80bc3d1867078a4997
Garrett <garrett@yellowcard.io>
2020-04-01 05:14:42 -0500

    added portfolio

---
+++ components/PortfolioItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card hover :href="link" class="mx-auto" max-width="400">
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="require('@/assets/images/' + image)"
+    >
+      <v-card-title :class="{ light: light }">{{ title }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">{{ subtitle }}</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      {{ content }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text :href="link">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String,
+    content: String,
+    image: String,
+    link: String,
+    light: Boolean
+  }
+}
+</script>
+
+<style scoped>
+.light {
+  color: black;
+}
+</style>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,5 +1,51 @@
+<template>
+  <v-container>
+    <v-layout ma-5 wrap>
+      <PortfolioItem
+        class="ma-5"
+        link="https://yellowcard.io/"
+        image="yellowcard.png"
+        title="YellowCard.io"
+        subtitle="Website for Yellow Card"
+        content="Statically generated website written in Nuxt.js for cryptocurrency excange service Yellow Card."
+      ></PortfolioItem>
+      <PortfolioItem
+        class="ma-5"
+        link="https://play.google.com/store/apps/details?id=io.yellowcard.app&hl=en"
+        image="yc-app.png"
+        title="Yellow Card Android App"
+        subtitle="Android port of the Yellow Card Web App"
+        content="Android app for Yellow Card transpiled for compatibility down to Android 5.0"
+        light
+      ></PortfolioItem>
+    </v-layout>
+    <v-layout ma-5 wrap>
+      <PortfolioItem
+        class="ma-5"
+        link="https://github.com/GarrettBlackmon/ranky"
+        image="ranky.png"
+        title="Ranky"
+        subtitle="Simple, Open-source, Discord Activity Ranking Bot"
+        content="Tiered activity ranking Discord bot written in NodeJS and SQLlite"
+      ></PortfolioItem>
+
+      <PortfolioItem
+        class="ma-5"
+        link="https://github.com/GarrettBlackmon/Angular-Todo"
+        image="todo.png"
+        title="Angular TODO"
+        subtitle="CRUD example project"
+        light
+        content="ToDo app written in Angular.js with Async/seperate client side handling for a good UX."
+      ></PortfolioItem>
+    </v-layout>
+  </v-container>
+</template>
+
 <script>
+import PortfolioItem from '@/components/PortfolioItem'
 export default {
+  components: { PortfolioItem },
   head() {
     return {
       title: 'Portfolio'
