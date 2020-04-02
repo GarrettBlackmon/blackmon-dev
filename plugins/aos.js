b3638a23050fde039f9b1a5b79c0b2afb21b1eb8
Garrett <garrett@yellowcard.io>
2020-04-01 23:09:18 -0500

    squashed theme change aos bug

--- plugins/aos.js
+++ plugins/aos.js
@@ -1,7 +1,8 @@
+import Vue from 'vue'
 import AOS from 'aos'
 
 import 'aos/dist/aos.css'
 
 export default ({ app }) => {
-  app.AOS = new AOS.init() // eslint-disable-line new-cap
+  app.AOS = new AOS.init({}) // eslint-disable-line new-cap
 }
